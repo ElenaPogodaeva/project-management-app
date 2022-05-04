90f2b4f6fc25ff75f0b6030febee724b1952e9d2
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2022-05-04 20:28:17 +0300

    feat: add default styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,23 @@ import './Header.scss';
 const Header = () => {
   return (
     <header className="header">
-      <div className="center-container">header</div>
+      <p className="home">home</p>
+      <div className="">
+        <p className="edit-profile">edit-profile</p>
+        <p className="new-board">new-board</p>
+      </div>
+      <div>
+        <select className="language">
+          <option value="en">en</option>
+          <option value="ru">ru</option>
+        </select>
+        <button type="button" className="login">
+          login
+        </button>
+        <button type="button" className="sign-up">
+          sign-up
+        </button>
+      </div>
     </header>
   );
 };
