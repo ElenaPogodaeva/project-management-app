c26c43c1adb49311ce5eaf7f8c9173f4684fef8e
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-05-09 22:42:23 +0300

    feat: add components for board page

--- src/views/Board/Board.tsx
+++ src/views/Board/Board.tsx
@@ -1,57 +1,35 @@
+import ColumnList from '../../components/ColumnList/ColumnList';
 import './Board.scss';
+
+const columns = [
+  {
+    id: '1',
+    title: 'Column1',
+    order: 1,
+  },
+  {
+    id: '2',
+    title: 'Column2',
+    order: 2,
+  },
+  {
+    id: '3',
+    title: 'Column3',
+    order: 3,
+  },
+  {
+    id: '4',
+    title: 'Column4',
+    order: 4,
+  },
+];
 
 const Board = () => {
   return (
     <div className="board">
       <div className="center-container">
         <section className="board-header">Board title</section>
-        <section className="column-list">
-          <div className="column-item">
-            <div className="column-title-wrapper">
-              <h3 className="column-title">Tasks to Do</h3>
-            </div>
-            <ul className="card-list">
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-            </ul>
-            <button type="button" className="add-card-btn">
-              Add a card...
-            </button>
-          </div>
-          <div className="column-item">
-            <div className="column-title-wrapper">
-              <h3 className="column-title">In progress</h3>
-            </div>
-            <ul className="card-list">
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-            </ul>
-          </div>
-          <div className="column-item">
-            <div className="column-title-wrapper">
-              <h3 className="column-title">Completed Tasks</h3>
-            </div>
-            <ul className="card-list">
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-              <li className="card-item">Lorem ipsum dolor sit.</li>
-            </ul>
-          </div>
-          <button type="button" className="add-column-btn">
-            Add a list...
-          </button>
-        </section>
+        <ColumnList columns={columns} />
       </div>
     </div>
   );
