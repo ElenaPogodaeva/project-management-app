ae2ef912a54c5e827463dff8b38a33afcc88b4e5
Alena Bulanova <kxzws54@gmail.com>
2022-05-10 02:23:35 +0300

    feat: add sticky style for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,52 +1,72 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import useActions from '../../hooks/useActions';
 import useTypedSelector from '../../hooks/useTypedSelector';
 import './Header.scss';
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
   const { isAuth } = useTypedSelector((state) => state.auth);
   const { logout } = useActions();
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setIsSticky(true);
+      }
+      if (window.scrollY < 11) {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
-      <NavLink to="/" className="header__btn btn-nav btn-home">
-        home
-      </NavLink>
-
-      <div>
-        <NavLink to="/editprofile" className="header__btn btn-nav">
-          edit-profile
+    <header className={`header ${isSticky ? 'header_sticky' : ''}`}>
+      <div className="center-container">
+        <NavLink to="/" className="header__btn btn-nav btn-home">
+          home
         </NavLink>
-        <NavLink to="/#" className="header__btn btn-nav">
-          new-board
-        </NavLink>
-      </div>
 
-      <div>
-        <select className="header__select">
-          <option value="en">en</option>
-          <option value="ru">ru</option>
-        </select>
-        {isAuth ? (
-          <button
-            type="button"
-            className="header__btn"
-            onClick={() => {
-              logout();
-            }}
-          >
-            log-out
-          </button>
-        ) : (
-          <>
-            <NavLink to="/login" className="header__btn btn-nav">
-              sign-in
-            </NavLink>
-            <NavLink to="/signup" className="header__btn btn-nav">
-              sign-up
-            </NavLink>
-          </>
-        )}
+        <div>
+          <NavLink to="/editprofile" className="header__btn btn-nav">
+            edit-profile
+          </NavLink>
+          <NavLink to="/#" className="header__btn btn-nav">
+            new-board
+          </NavLink>
+        </div>
+
+        <div>
+          <select className="header__select">
+            <option value="en">en</option>
+            <option value="ru">ru</option>
+          </select>
+          {isAuth ? (
+            <button
+              type="button"
+              className="header__btn"
+              onClick={() => {
+                logout();
+              }}
+            >
+              log-out
+            </button>
+          ) : (
+            <>
+              <NavLink to="/login" className="header__btn btn-nav">
+                sign-in
+              </NavLink>
+              <NavLink to="/signup" className="header__btn btn-nav">
+                sign-up
+              </NavLink>
+            </>
+          )}
+        </div>
       </div>
     </header>
   );
