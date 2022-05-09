1d061a8ea2fc7c42db09c466936c2207b1fa9b1b
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-05-10 02:52:17 +0300

    feat: add redux store and slice

---
+++ src/redux/thunks.ts
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { api } from '..';
+import { ICreateColumn } from '../model/interfaces';
+
+export const addColumn = createAsyncThunk(
+  'board/addColumn',
+  async (columnData: { boardId: string; column: ICreateColumn }, { rejectWithValue }) => {
+    try {
+      const { boardId, column } = columnData;
+      const response = await api.createColumn(boardId, column);
+      return response;
+    } catch (err) {
+      return rejectWithValue((err as Error).message);
+    }
+  }
+);
