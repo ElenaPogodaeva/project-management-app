e3cd2908394c097d64e8510432d55d067b87e1c6
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-05-10 13:31:01 +0300

    feat: add boardReducer

--- src/redux/reducers/reducer.ts
+++ src/redux/reducers/reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import settingsReducer from './settingsReducer';
+import boardReducer from '../features/boardSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   settings: settingsReducer,
+  board: boardReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
