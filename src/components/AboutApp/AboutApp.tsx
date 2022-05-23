bbdc2142dfbccc0810bf8778fa3474026b752fc8
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2022-05-23 21:29:02 +0300

    feat: add styles to about app section

--- src/components/AboutApp/AboutApp.tsx
+++ src/components/AboutApp/AboutApp.tsx
@@ -1,10 +1,45 @@
 import './AboutApp.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const AboutApp = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000, offset: 200 });
+  }, []);
+
   return (
     <section className="about-app">
       <h1 className="title">About App</h1>
-      <p>пока пусть будет пустое поле для творчества. ближе к концу чем-нибудь заполним</p>
+      <div className="about-app__card" data-aos="fade-up">
+        <img src="" alt="" className="card-img" />
+        <article>
+          <h3>Your new project organizer</h3>
+          <p className="description">
+            A new way to ad your team to the board, add cards with information and have a full view
+            of what you have done and has to do
+          </p>
+        </article>
+      </div>
+      <div className="about-app__card" data-aos="fade-up">
+        <article>
+          <h3>Board</h3>
+          <p className="description">
+            It&apos;s possible to change the name of the column by editing, delete column and move
+            them to another position. You can also add as many columns as you need
+          </p>
+        </article>
+        <img src="" alt="" className="card-img" />
+      </div>
+      <div className="about-app__card" data-aos="fade-up">
+        <img src="" alt="" className="card-img" />
+        <article>
+          <h3>Details</h3>
+          <p className="description">
+            A drag and drop system is used to move a cards between the columns
+          </p>
+        </article>
+      </div>
     </section>
   );
 };
